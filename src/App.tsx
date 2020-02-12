ef38e279fe71ddf834f34113ff3878977ce11ac2
Paul Johnson <paul.johnson@prospectsoft.com>
2020-02-12 10:17:12 +0000

    Reworked example because it looked bad

--- src/App.tsx
+++ src/App.tsx
@@ -2,101 +2,152 @@ import React, { useEffect, useState } from "react";
 import "./App.scss";
 
 const App = () => {
-    const [photos, setPhotos] = useState<Record<string, any>[]>([]);
-    const [photo, setPhoto] = useState("");
+    const [users, setUsers] = useState<Record<string, any>[]>([]);
+    const [user, setUser] = useState<Record<string, any> | null>(null);
 
     useEffect(() => {
-        fetch("/photos").then((res) => {
+        fetch("/users").then((res) => {
             if (res.status === 200) {
-                res.json().then((json) => setPhotos(json));
+                res.json().then((json) => setUsers(json));
             }
         });
     });
 
-    async function selectPhoto(photoId: string) {
-        const res = await fetch(`/photos/${photoId}`);
+    async function selectUser(userId: string) {
+        const res = await fetch(`/users/${userId}`);
         if (res.status === 200) {
-            const photo = await res.json();
-            setPhoto(photo.src);
+            const user = await res.json();
+            setUser(user);
         }
     }
 
     return (
-        <div className="app">
-            <div className="card title-card">
-                <h1>Pretender Demo</h1>
-                <span>This app is making HTTP calls, but they are intercepted by the Pretender server.</span>
-                <span>
-                    Checkout <code>server.ts</code> to see how it works.
-                </span>
-                <pre>
-                    {`//// server.ts ////
+        <main>
+            <section className="section">
+                <div className="card">
+                    <div className="card-content">
+                        <h1 className="title">How does it work?</h1>
+                        <div className="columns is-mobile is-centered">
+                            <div className="column">
+                                <h2 className="title">1. Prep your server</h2>
+                                <pre>
+                                    {`import Pretender from "pretender";
 
-import Pretender from "pretender";
-
-const PHOTOS: Record<string, { id: number, src: string }> = {
-    "10": {
-        id: 10,
-        src: "https://media.giphy.com/media/UdqUo8xvEcvgA/giphy.gif"
+const USERS: Record<string, { id: number, name: string, role: string, avatar: string }> = {
+    "12345": {
+        id: 12345,
+        name: "Ned",
+        role: "Read Only User",
+        avatar: "some url"
     },
-    "42": {
-        id: 42,
-        src: "https://media0.giphy.com/media/Ko2pyD26RdYRi/giphy.gif"
+    "54321": {
+        id: 12345,
+        name: "Bob",
+        role: "Read Write User",
+        avatar: "another url"
     }
 }
 
 export default function createServer() {
     const server = new Pretender(function () {
-        this.get("/photos", () => {
-            const all = JSON.stringify(Object.keys(PHOTOS).map(k => PHOTOS[k]));
+        this.get("/users", () => {
+            const all = JSON.stringify(Object.keys(USERS).map(k => USERS[k]));
             return [200, { "Content-Type": "application/json" }, all];
         })
 
-        this.get("/photos/:id", request => {
+        this.get("/users/:id", request => {
             const idParameter: string = (request.params as any).id;
-            const photo = JSON.stringify(PHOTOS[idParameter]);
-            return [200, { "Content-Type": "application/json" }, photo];
+            const user = JSON.stringify(USERS[idParameter]);
+            return [200, { "Content-Type": "application/json" }, user];
         })
     });
 
     return server;
 }`}
-                </pre>
-                <span>
-                    If you have the network tab open, you'll see our calls to{" "}
-                    <em>
-                        <strong>/photos</strong>
-                    </em>{" "}
-                    and{" "}
-                    <em>
-                        <strong>/photos/:id</strong>
-                    </em>{" "}
-                    are not listed.
-                </span>
-                <span>They have been intercepted before hitting the standard XHTTP request handler.</span>
-            </div>
-            <div className="card demo-card">
-                <ul>
-                    {photos.map((p) => (
-                        <li onClick={() => selectPhoto(p.id)} key={p.id}>
-                            {p.id}
-                        </li>
-                    ))}
-                </ul>
-                {photo ? (
-                    <div>
-                        <img src={photo} alt={photo} />
+                                </pre>
+                            </div>
+                            <div className="column">
+                                <h2 className="title">2. Import it during dev</h2>
+                                <pre>
+                                    {`import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import makeServer from "./server";
+
+if (process.env.NODE_ENV === "development") {
+    makeServer();
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+`}
+                                </pre>
+                                <hr></hr>
+                                <h2 className="title">3. That's it!</h2>
+                                <p>
+                                    Your HTTP calls to <strong>/users</strong> and <strong>/users/:id</strong> are now
+                                    being intercepted and handled by the mock server
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                ) : (
-                    <></>
-                )}
-            </div>
-            <div className="card footer-card">
-                <span>Checkout the </span>
-                <a href="https://github.com/pretenderjs/pretender">Pretender Docs</a>
-                <span> for more info</span>
-            </div>
-        </div>
+                </div>
+            </section>
+            <section className="section">
+                <div className="container">
+                    <div className="card">
+                        <div className="card-content">
+                            <div className="columns">
+                                <div className="column">
+                                    <h2 className="title">Here's a simple example</h2>
+                                    <div className="panel">
+                                        <p className="panel-heading">Users</p>
+                                        {users.map((u) => (
+                                            <a className="panel-block" key={u.id} onClick={() => selectUser(u.id)}>
+                                                <span className="panel-icon">
+                                                    <i className="fas fa-user" aria-hidden="true"></i>
+                                                </span>
+                                                <span>
+                                                    {u.name} - {u.role}
+                                                </span>
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className="column">
+                                    <div className="panel">
+                                        {user ? (
+                                            <>
+                                                <p className="panel-heading">
+                                                    {user.name} - {user.role}
+                                                </p>
+                                                <div className="panel-block">
+                                                    <div className="container">
+                                                        <img
+                                                            src={user.avatar}
+                                                            alt={user.name}
+                                                            style={{ width: "100%" }}
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </>
+                                        ) : (
+                                            <></>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,31 @@
 import Pretender from "pretender";
 
-const PHOTOS: Record<string, { id: number, src: string }> = {
-    "10": {
-        id: 10,
-        src: "https://media.giphy.com/media/UdqUo8xvEcvgA/giphy.gif"
+const USERS: Record<string, { id: number, name: string, role: string, avatar: string }> = {
+    "12345": {
+        id: 12345,
+        name: "Ned",
+        role: "Read Only User",
+        avatar: "https://vignette.wikia.nocookie.net/gameofthrones/images/3/34/Eddard_Stark.jpg/revision/latest/top-crop/width/360/height/360?cb=20190701140812"
     },
-    "42": {
-        id: 42,
-        src: "https://media0.giphy.com/media/Ko2pyD26RdYRi/giphy.gif"
+    "54321": {
+        id: 54321,
+        name: "Bob",
+        role: "Read Write User",
+        avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Nicolas_Cage_Deauville_2013.jpg/220px-Nicolas_Cage_Deauville_2013.jpg"
     }
 }
 
 export default function createServer() {
     const server = new Pretender(function () {
-        this.get("/photos", () => {
-            const all = JSON.stringify(Object.keys(PHOTOS).map(k => PHOTOS[k]));
+        this.get("/users", () => {
+            const all = JSON.stringify(Object.keys(USERS).map(k => USERS[k]));
             return [200, { "Content-Type": "application/json" }, all];
         })
 
-        this.get("/photos/:id", request => {
+        this.get("/users/:id", request => {
             const idParameter: string = (request.params as any).id;
-            const photo = JSON.stringify(PHOTOS[idParameter]);
-            return [200, { "Content-Type": "application/json" }, photo];
+            const user = JSON.stringify(USERS[idParameter]);
+            return [200, { "Content-Type": "application/json" }, user];
         })
     });
 
